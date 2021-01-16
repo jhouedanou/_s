a86a81876a312616ffcbf2a6f9b54e21be17e790
Jean Luc Houedanou <jhouedanou@gmail.com>
2021-01-16 16:59:03 +0000

    gulp

---
+++ functions.js
@@ -0,0 +1,39 @@
+  AOS.init();
+function init() {
+var vidDefer = document.getElementsByTagName('iframe');
+for (var i=0; i<vidDefer.length; i++) {
+if(vidDefer[i].getAttribute('data-src')) {
+vidDefer[i].setAttribute('src',vidDefer[i].getAttribute('data-src'));
+} } }
+window.onload = init;
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+(function($) {
+ 	$(window).on('load', function () {
+    
+    });
+    
+    $(document).ready(function() {
+  
+    });
+
+}(window.jQuery || window.$));
+
